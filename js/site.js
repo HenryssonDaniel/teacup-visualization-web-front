e7d11eda709c87b866d5468996740f9a731f1f8e
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-05-29 16:24:54 -0400

    Set with credentials to support cookies

--- js/site.js
+++ js/site.js
@@ -63,6 +63,7 @@ function logIn(element) {
 
     xmlHttpRequest.open('POST', `${location.protocol}//${location.hostname}:5000/api/account/logIn`, true);
     xmlHttpRequest.setRequestHeader('Content-type', 'text/plain');
+    xmlHttpRequest.withCredentials = true;
     xmlHttpRequest.send(`{"email":"${element.querySelector('input[name="email"]').value}", 
     "password":"${element.querySelector('input[name="password"]').value}"}`);
 }
